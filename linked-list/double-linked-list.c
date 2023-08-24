b6d151413e0467b9d27e4510d9f4a9fb5ead2550
Arthur Saito <arthur_saito1998@hotmail.com>
2023-08-23 23:44:48 -0300

    feat: adding push method to double linked list

--- linked-list/double-linked-list.c
+++ linked-list/double-linked-list.c
@@ -36,3 +36,48 @@ int main()
     free(linkedList);
     return 0;
 }
+
+int isEmpty(node *linkedList)
+{
+    if (linkedList->next == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void push(node *linkedList)
+{
+    // Space for a newNode
+    node *newNode = (node *)malloc(sizeof(node));
+
+    // Verify if there is memory to a newNode
+    if (!newNode)
+    {
+        printf("There's no available memory.\n");
+        exit(1);
+    }
+    printf("value to push: ");
+    scanf("%d", &newNode->value);
+
+    // Check if it's the list is empty. if is empty, so there's no next Node.
+    if (isEmpty(linkedList))
+    {
+        linkedList->next = newNode;
+        newNode->next = NULL;
+        newNode->prev = linkedList;
+    }
+    else
+    {
+        // If it's not empty, is necessary update the pointers to correct nodes.
+        node *oldHead = linkedList->next;
+
+        linkedList->next = newNode;
+        newNode->next = oldHead;
+        newNode->prev = linkedList;
+        oldHead->prev = newNode;
+    }
+}
