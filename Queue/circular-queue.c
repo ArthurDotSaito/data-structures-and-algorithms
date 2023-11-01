b6622c3229af50890e5f56c3222ebf64d4528c5d
Arthur Saito <arthur_saito1998@hotmail.com>
2023-10-31 22:40:09 -0300

    feat adding enqueue on circular queue

--- Queue/circular-queue.c
+++ Queue/circular-queue.c
@@ -3,13 +3,35 @@
 
 #define SIZE 8
 
-int Queue[SIZE];
+int queue[SIZE];
 
 int quantity, head, tail;
+
+void start();
+void enqueue(char c);
 
 void start()
 {
     quantity = 0;
     head = 0;
     tail = 0;
+}
+
+void enqueue(char c)
+{
+    if (!full())
+    {
+        queue[tail] = c;
+        tail++;
+        quantity++;
+
+        if (tail == SIZE)
+        {
+            tail = 0;
+        }
+        else
+        {
+            printf("full queue");
+        }
+    }
 }
